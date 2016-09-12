e835770fcb8fa80f8d1de8072a08c5d2b09a6433
Brandon Tribble <brandontribble85@gmail.com>
2016-09-11 20:48:34 -0500

    Adding README for the peeps
    Adding Readme, and updating the style for the main elm file, using elm-format.

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -1,4 +1,5 @@
 module PhotoGroove exposing (..)
+
 import Array exposing (Array)
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -6,27 +7,31 @@ import Html.Events exposing (onClick)
 import Html.App
 import Random
 
+
 urlPrefix : String
-urlPrefix = "http://elm-in-action.com/"
+urlPrefix =
+    "http://elm-in-action.com/"
+
 
 type ThumbnailSize
     = Small
     | Medium
     | Large
 
-view
-    : Model
+
+view :
+    Model
     -> Html Msg
-view model = 
-  div [ class "content" ]
+view model =
+    div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , button 
+        , button
             [ onClick SurpriseMe ]
             [ text "Surprise Me!" ]
         , h3 [] [ text "Thumbnail size" ]
         , div [ id "choose-size" ]
             (List.map viewSizeChooser [ Small, Medium, Large ])
-        , div [ id "thumbnails", class (sizeToString model.chosenSize) ] 
+        , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
             [ class "large"
@@ -35,58 +40,68 @@ view model =
             []
         ]
 
-viewThumbnail 
-    : String
+
+viewThumbnail :
+    String
     -> Photo
     -> Html Msg
-
-viewThumbnail selectedUrl thumbnail = 
+viewThumbnail selectedUrl thumbnail =
     img
-        [ src (urlPrefix ++ thumbnail.url) 
+        [ src (urlPrefix ++ thumbnail.url)
         , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
         , onClick (SelectByUrl thumbnail.url)
-        ] []
+        ]
+        []
+
 
 viewSizeChooser : ThumbnailSize -> Html Msg
-viewSizeChooser size = 
+viewSizeChooser size =
     label []
         [ input [ type' "radio", name "size", onClick (SetSize size) ] []
         , text (sizeToString size)
         ]
 
+
 sizeToString : ThumbnailSize -> String
-sizeToString size = 
+sizeToString size =
     case size of
         Small ->
             "small"
-        Medium -> 
+
+        Medium ->
             "medium"
+
         Large ->
             "large"
 
-type alias Photo = 
-    { url: String }
+
+type alias Photo =
+    { url : String }
+
 
 type alias Model =
-    { photos: List Photo
+    { photos : List Photo
     , selectedUrl : String
-    , chosenSize: ThumbnailSize
+    , chosenSize : ThumbnailSize
     }
 
+
 initialModel : Model
-initialModel = 
-    { photos = 
-        [ { url = "1.jpeg"}
-        , { url = "2.jpeg"}
-        , { url = "3.jpeg"}
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
     , chosenSize = Medium
     }
 
+
 photoArray : Array Photo
 photoArray =
     Array.fromList initialModel.photos
+
 
 getPhotoUrl : Int -> String
 getPhotoUrl index =
@@ -97,36 +112,42 @@ getPhotoUrl index =
         Nothing ->
             ""
 
+
 randomPhotoPicker : Random.Generator Int
 randomPhotoPicker =
     Random.int 0 (Array.length photoArray - 1)
 
+
 type Msg
     = SelectByUrl String
-    | SelectByIndex Int 
+    | SelectByIndex Int
     | SurpriseMe
     | SetSize ThumbnailSize
 
-update
-    : Msg
-    -> Model -> (Model, Cmd Msg)
-    
-update msg model = 
+
+update :
+    Msg
+    -> Model
+    -> ( Model, Cmd Msg )
+update msg model =
     case msg of
         SelectByUrl url ->
             ( { model | selectedUrl = url }, Cmd.none )
+
         SurpriseMe ->
-            (  model, Random.generate SelectByIndex randomPhotoPicker )
+            ( model, Random.generate SelectByIndex randomPhotoPicker )
+
         SetSize size ->
             ( { model | chosenSize = size }, Cmd.none )
-        SelectByIndex index -> 
-            ( { model | selectedUrl = getPhotoUrl index}, Cmd.none )
-        
+
+        SelectByIndex index ->
+            ( { model | selectedUrl = getPhotoUrl index }, Cmd.none )
+
 
 main : Program Never
-main = 
+main =
     Html.App.program
-        { init = ( initialModel, Cmd.none)
+        { init = ( initialModel, Cmd.none )
         , view = view
         , update = update
         , subscriptions = (\model -> Sub.none)
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-PhotoGroobe
+PhotoGroove : About
+===================
+
+This is a project that is based off of the so far very very good "Elm in Action" by Richard Feldman available from Manning Publications.
+
+In this project, there is an interface which contains a gallery of photos, and a larger view of a selected Photo.  Other features include : 
+* Random Photo Picker
+* Thumbnail Size Picker
+
+Setup
+-----
+
+to get started using elm I.E. setting up your local environment, please follow the guidance set [here](http://guide.elm-lang.org/get_started.html "Get Started with Elm")
+
+I also suggest that you set your editor of choice up for development in Elm.  I personally have found the experience in VScode to be
+wonderful, largely because you get compiler feedback every time you save a file.  Although, I've read people say good things about most of the
+more popular text-editors.  Atom seems to be fairly popular right now. 
+
+I would further suggest that you use elm-format.  It will 
+help you to quickly understand what the community 'best practice' or style is fairly quickly.  It also does the work for you -- if you have any
+confusion over what happened, looking at the diff is extremely helpful. 
+
+information for elm-format can be found [here](https://github.com/avh4/elm-format#installation "elm-format")
+
+Compiling/Other
+---------------
+
+### A note on compiling elm. 
+
+You will need to compile this code in order for you changes to take effect.  For example, right now since all the elm code that we are writing is 
+maintained in a single file, the command is:
+
+_elm-make PhotoGroove.elm --output elm.js_
+
+this simply takes the file PhotoGroove.elm (our elm source) and creates a compiled output of elm.js which we use in index.html
+
+Beyond that, learn a little about types, type alias(ing) in elm, and general language features, and above all else, have fun!  
